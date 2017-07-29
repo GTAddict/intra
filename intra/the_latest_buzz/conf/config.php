be41738332f7d59ea2a6c67987a84e852289ec0a
GTAddict <soregtaddict@gmail.com>
2017-07-29 13:12:15 -0400

    Added more files that were missed

---
+++ intra/the_latest_buzz/conf/config.php
@@ -0,0 +1,17 @@
+<?php if (!defined('APPLICATION')) exit();
+
+// Database
+$Configuration['Database']['Name'] = '';		// Name removed for internet upload
+$Configuration['Database']['Host'] = '';		// Host removed for internet upload
+$Configuration['Database']['User'] = '';		// User removed for internet upload
+$Configuration['Database']['Password'] = '';	// Password removed for internet upload
+
+// Garden
+$Configuration['Garden']['Title'] = 'Intra Forums';
+$Configuration['Garden']['Cookie']['Salt'] = '';				// Salt removed for internet upload
+$Configuration['Garden']['Cookie']['Domain'] = '';				// Domain removed for internet upload
+$Configuration['Garden']['Registration']['ConfirmEmail'] = TRUE;
+$Configuration['Garden']['Email']['SupportName'] = 'Intra Forums';
+$Configuration['Garden']['Installed'] = TRUE;
+
+// Last edited by gtaddict (127.0.0.1)2012-07-09 18:21:25
